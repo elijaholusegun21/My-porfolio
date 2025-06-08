72b4beae4a04d0577d68fa99a3a572d0f0b31083
elijaholusegun21 <elijaholusegun21@gmail.com>
2025-06-08 18:37:30 +0100

    theme toggle updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,9 +33,14 @@ const AboutSection = () => {
               {" "}
               Get in Touch
             </a>
-            <a href="" className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300' >
-            {" "}
-            Download CV
+            <a 
+              href="https://drive.google.com/file/d/1GXiUipXPolcM8EHdproOYblLNLNi4pq5/view?usp=drivesdk"
+              target='_blank'
+              rel='noopener noreferrer'
+              className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300'
+            >
+              {" "}
+              Download CV
             </a>
           </div>
         </div>

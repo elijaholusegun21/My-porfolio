65a9b1ca0648dffc8003f53bf0641c192da748b8
elijaholusegun21 <elijaholusegun21@gmail.com>
2025-06-14 11:44:56 +0100

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer= () => {
   return (
-    <footer className='py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center'>
+    <footer className='py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-center items-center'>
       {" "}
       <p className='text-sm text-muted-foreground'>
 

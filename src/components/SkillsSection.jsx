c85c0ae354f62362550c1d4e5581c79be4dc8bfe
elijaholusegun21 <elijaholusegun21@gmail.com>
2025-08-17 21:13:54 +0100

    updated contact section

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -68,7 +68,7 @@ const SkillsSection = () => {
                     <div className='w-full bg-secondary/50 h-2 rounded-full overflow-hidden'>
                         <div className='bg-primary h-2 rounded-full origin-left animate-[grow_1.5s_ease-out'
                          />
-                    </div>
+                    </div>pdated 
                 </div>
             ))}
         </div>
